c0c445186922b4bf1386f88aff4ecd16851f2453
radixy0 <dummy>
2025-10-05 20:45:58 +0200

    shuffle some files around, add wsl config

---
+++ wsl/windows_userhome/.config/wezterm/wezterm.lua
@@ -0,0 +1,31 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+function scheme_for_appearance(appearance)
+  if appearance:find "Dark" then
+    return "Catppuccin Mocha"
+  else
+    return "Catppuccin Latte"
+  end
+end
+
+-- SHELL
+config.default_prog = { 'pwsh' }
+config.default_cwd = "C:\\Development"
+
+-- COLOR AND FONT
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+config.font = wezterm.font { family = 'CommitMono Nerd Font', weight = 'Regular' }
+
+
+-- WINDOW
+config.window_background_opacity = 0.95
+config.window_decorations = "RESIZE"
+
+
+-- TAB BAR
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
+
+return config
